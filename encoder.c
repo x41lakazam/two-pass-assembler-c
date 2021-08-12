f00f7a72dfc62d4672fca92436e7efe3588a34d1
David Sariel <davidsa@openu.ac.il>
2021-08-12 13:53:30 -0400

    add dump_bitmap

---
+++ encoder.c
@@ -0,0 +1,18 @@
+#include "encoder.h"
+
+void dump_bitmap(BITMAP_32 *bitmap, FILE *f) {
+/* assuming that f is already open */
+	int i; 
+	unsigned int j; 
+	
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 1 << 8; j > 0; j = j / 2)
+		{
+			(bitmap[i] & j) ? fprintf(f, "1") : fprintf(f, "0");
+		}
+		fprintf(f, "\n");
+	}
+}
+
+
