62c3ac3cc54ae0382feec9ec03b7b63d178b20d7
x41lakzaam <x41lakazam@gmail.com>
2021-08-17 15:08:21 +0300

    Changed dump bitmap

--- encoder.c
+++ encoder.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include "encoder.h"
 
-void dump_bitmap(BITMAP_32 *bitmap, FILE *f) {
+void dump_bitmap(BITMAP_32 *bitmap, char *fname, int line_no) {
 /* assuming that f is already open */
 	int i;
 	unsigned int j;
+    FILE *fp;
+
+    fp = fopen(fname, "a");
 
 	for (i = 0; i < 4; i++)
 	{
 		for (j = 1 << 8; j > 0; j = j / 2)
 		{
-			(*bitmap[i] & j) ? fprintf(f, "1") : fprintf(f, "0");
+			(*bitmap[i] & j) ? fprintf(fp, "1") : fprintf(fp, "0");
 		}
-		fprintf(f, "\n");
+
+    fclose(fp);
 	}
 }
 
